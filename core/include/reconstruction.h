370f3f5c595ad6e6d458ddf6c812d05e6780b9df
Kyle Olodin <kylemartinezolodin@gmail.com>
2022-05-05 07:25:50 +0800

    Added Poisson Reconstruction Method utilizing PCL

--- core/include/reconstruction.h
+++ core/include/reconstruction.h
@@ -1,9 +1,32 @@
 #ifndef RECONSTRUCTION
 #define RECONSTRUCTION
 
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+
+#include <pcl/io/ply_io.h>
+
+#include <pcl/point_types.h>
+
+#include <pcl/common/common.h>
+
+#include <pcl/search/kdtree.h>
+
+#include <pcl/PolygonMesh.h>
+
+#include <pcl/filters/passthrough.h>
+
+#include <pcl/features/normal_3d_omp.h>
+
+#include <pcl/surface/poisson.h>
+
 namespace _3DRPCore {
 	namespace Reconstruction {
 		std::string print_something();
+
+		//pcl::PolygonMesh poisson(pcl::PointCloud<pcl::PointXYZ>::Ptr inputCloud);
+		pcl::PolygonMesh poisson(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud);
+		//pcl::PolygonMesh poisson(pcl::PointCloud<pcl::PointNormal>::Ptr pointcloudWithNormals); // THIS FUNCTION WILL BE A CONTAINER WHERE AN ACTUAL RECONSTRUCTION HAPPENS
 	}
 }
 
--- core/src/reconstruction.cpp
+++ core/src/reconstruction.cpp
@@ -1,6 +1,63 @@
 #include <iostream>
 #include "reconstruction.h"
 
+
+using namespace std;
+//using namespace pcl;
+
 std::string _3DRPCore::Reconstruction::print_something() {
 	return "reconstructing..";
+}
+
+//pcl::PolygonMesh poisson(pcl::PointCloud<pcl::PointNormal>::Ptr pointcloudWithNormals) {
+//	pcl::PolygonMesh a;
+//	return a;
+//}
+ 
+pcl::PolygonMesh _3DRPCore::Reconstruction::poisson(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud) {
+
+    // passthrough filter
+    pcl::PointCloud<pcl::PointXYZ>::Ptr filtered(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::PassThrough<pcl::PointXYZ> filter;
+    filter.setInputCloud(cloud);
+    filter.filter(*filtered);
+
+    // estimate normal
+    pcl::NormalEstimationOMP<pcl::PointXYZ, pcl::Normal> ne;
+    ne.setNumberOfThreads(8);
+    ne.setInputCloud(filtered);
+
+    // Create an empty kdtree representation, and pass it to the normal estimation object.
+    // Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>());
+    ne.setSearchMethod(tree);
+
+    ne.setRadiusSearch(0.01);
+    //ne.setRadiusSearch(0.1);
+    Eigen::Vector4f centroid;
+    compute3DCentroid(*filtered, centroid);
+    ne.setViewPoint(centroid[0], centroid[1], centroid[2]);
+
+    pcl::PointCloud<pcl::Normal>::Ptr cloud_normals(new pcl::PointCloud<pcl::Normal>());
+    ne.compute(*cloud_normals);
+
+    // flip normal
+    for (size_t i = 0; i < cloud_normals->size(); ++i) {
+        cloud_normals->points[i].normal_x *= -1;
+        cloud_normals->points[i].normal_y *= -1;
+        cloud_normals->points[i].normal_z *= -1;
+    }
+
+    // combine cloud point and estimated normal
+    pcl::PointCloud<pcl::PointNormal>::Ptr cloud_smoothed_normals(new pcl::PointCloud<pcl::PointNormal>());
+    pcl::concatenateFields(*filtered, *cloud_normals, *cloud_smoothed_normals);
+
+    // cloud w/ normals poisson reconstruction into mesh
+    pcl::Poisson<pcl::PointNormal> poisson;
+    poisson.setDepth(9);
+    poisson.setInputCloud(cloud_smoothed_normals);
+    pcl::PolygonMesh mesh;
+    poisson.reconstruct(mesh);
+
+    return mesh;
 }
